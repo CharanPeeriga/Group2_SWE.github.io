56eaa6a15454ebc299d7202fbdf7647b77953c08
Daisy Holt <dbell61804@gmail.com>
2025-03-22 20:17:15 -0400

    Create registrationscript.js

---
+++ public/registrationscript.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("regModal");
+    const closeButton = document.getElementById("closeModal");
+
+    //add user to user database portal
+}
