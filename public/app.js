d2fc3cc9a3645ce3ef825769e721468c946c0049
U-BAILSPC\baile <baileyreese123@gmail.com>
2025-03-27 17:51:20 -0400

    Login/Register Button on Index (Home) Page only shows if user is not logged in

--- public/app.js
+++ public/app.js
@@ -1,3 +1,36 @@
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"; // Use the same version here
+import { firebaseConfig } from "./firebase-config.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+const loginRegisterItem = document.getElementById("loginRegisterItem");
+// Check if the user is logged in or not
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in
+        console.log("User is logged in:", user);
+        // Hide the login/register list item if the user is logged in
+        if (loginRegisterItem) {
+            loginRegisterItem.style.display = "none";
+        }
+    } else {
+        // User is not signed in
+        console.log("No user is logged in.");
+        // Show the login/register list item if the user is not logged in
+        if (loginRegisterItem) {
+            loginRegisterItem.style.display = "block";
+        }
+    }
+});
+
 document.addEventListener('DOMContentLoaded', function() 
 {
     const eventQuery = new URLSearchParams(window.location.search).get('event');
